79560e102c1035747fe2597b7f0a8ae0ce54ea59
Omar Trejo <omar@mllabs.com>
2022-08-23 02:03:57 -0500

    Latest changes

--- tmux/envs/ayr.sh
+++ tmux/envs/ayr.sh
@@ -17,32 +17,31 @@ tmux new-window -t $S -a -n azure-vm
 tmux send-keys -t $S:2 "az ssh vm --ip 10.0.0.4" Enter
 tmux_nested $S 2
 
-# This VM has been deleted
-# tmux new-window -t $S -a -n aws-vm
-# tmux send-keys -t $S:3 "cd ~/code/pm/ayr/" Enter
-# tmux send-keys -t $S:3 "ssh -i ./aws/recommendation-engine.pem ubuntu@3.143.251.76" Enter
-# tmux_nested $S 3
-
 tmux new-window -t $S -a -n db
 tmux send-keys -t $S:3 "cd ~/code/pm/ayr/" Enter
 tmux send-keys -t $S:3 "psql -h 10.0.0.5 -d ayr-surfside -U omarpocketmade@ayrdwpoc" Enter
 
-tmux new-window -t $S -a -n local-0
-tmux send-keys -t $S:4 "cd ~/code/pm/ayr/prev/" Enter
-tmux_env_python $S 4
+tmux new-window -t $S -a -n aws-vm
+tmux send-keys -t $S:4 "cd ~/code/pm/ayr/" Enter
+tmux send-keys -t $S:4 "ssh ubuntu@18.217.167.102" Enter
+tmux_nested $S 4
 
-tmux new-window -t $S -a -n local-1
-tmux send-keys -t $S:5 "cd ~/code/pm/ayr/ayr-website-connectors/" Enter
+tmux new-window -t $S -a -n local-0
+tmux send-keys -t $S:5 "cd ~/code/pm/ayr/prev/" Enter
 tmux_env_python $S 5
 
-tmux new-window -t $S -a -n local-2
+tmux new-window -t $S -a -n local-1
 tmux send-keys -t $S:6 "cd ~/code/pm/ayr/ayr-website-connectors/" Enter
 tmux_env_python $S 6
 
-tmux new-window -t $S -a -n emacs
-tmux send-keys -t $S:7 "cd ~/code/pm/ayr/" Enter
+tmux new-window -t $S -a -n local-2
+tmux send-keys -t $S:7 "cd ~/code/pm/ayr/ayr-website-connectors/" Enter
 tmux_env_python $S 7
-tmux_emacs $S 7
 
-tmux select-window -t $S:7
+tmux new-window -t $S -a -n emacs
+tmux send-keys -t $S:8 "cd ~/code/pm/ayr/" Enter
+tmux_env_python $S 8
+tmux_emacs $S 8
+
+tmux select-window -t $S:8
 tmux attach -t $S
