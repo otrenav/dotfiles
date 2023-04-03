bd68c2bbee1dd66402bd46a0f80fe8549b4b0ad7
Omar Trejo <omar@mllabs.com>
2023-04-03 13:49:24 -0600

    Latest changes

--- tmux/envs/ggstr.sh
+++ tmux/envs/ggstr.sh
@@ -10,12 +10,27 @@ export S="ggstr"
 tmux new-session -d -s $S
 tmux_email_ggstr $S
 
-tmux rename-window -t $S:1 vm
+tmux rename-window -t $S:1 aws-vm
 tmux send-keys -t $S:1 "cd ~/code/ggstr/ggstr/platform" Enter
 tmux send-keys -t $S:1 "
   ssh -i 'auth/omar-trejo.pem' ubuntu@ec2-44-210-134-74.compute-1.amazonaws.com
 " Enter
 tmux_nested $S 1
+
+tmux new-window -t $S -a -n aws-ssh-tunnel
+tmux send-keys -t $S:2 "cd ~/code/ggstr/ggstr/platform" Enter
+tmux send-keys -t $S:2 "
+  ssh -i 'auth/omar-trejo.pem' ubuntu@ec2-44-210-134-74.compute-1.amazonaws.com \
+      -L 5433:gigster-data-system.cupup2fqtumo.us-east-1.rds.amazonaws.com:5432 \
+      -N -p 22
+" Enter
+
+tmux new-window -t $S -a -n aws-db
+tmux send-keys -t $S:3 "cd ~/code/ggstr/ggstr/platform" Enter
+tmux send-keys -t $S:3 "
+  psql --host=localhost --port=5433 --user=data_system \
+       --dbname=gigster_data_system --password
+" Enter
 
 tmux new-window -t $S -a -n local-db
 tmux send-keys -t $S:2 "cd ~/code/ggstr/ggstr/platform" Enter
