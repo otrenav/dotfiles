225e8f77068307059483cec604697afe5d76bfae
Omar Trejo <omar@mllabs.com>
2024-08-13 10:52:30 -0600

    Latest changes

--- tmux/envs/alpha.sh
+++ tmux/envs/alpha.sh
@@ -9,18 +9,22 @@ export S="alpha"
 tmux new-session -d -s $S
 tmux_email_ggstr $S
 
-tmux rename-window -t $S:1 lcl-1
-tmux send-keys -t $S:1 "cd ~/code/ggstr/alpha/" Enter
+tmux rename-window -t $S:1 simulator
+tmux send-keys -t $S:1 "cd ~/code/ggstr/alpha/svc-simulator" Enter
 tmux_env_python $S 1
 
-tmux new-window -t $S -a -n lcl-2
-tmux send-keys -t $S:2 "cd ~/code/ggstr/alpha/" Enter
+tmux new-window -t $S -a -n analyzer
+tmux send-keys -t $S:2 "cd ~/code/ggstr/alpha/svc-analyzer" Enter
 tmux_env_python $S 2
 
-tmux new-window -t $S -a -n emacs
+tmux new-window -t $S -a -n lcl-1
 tmux send-keys -t $S:3 "cd ~/code/ggstr/alpha/" Enter
 tmux_env_python $S 3
-tmux_emacs $S 3
 
-tmux select-window -t $S:3
+tmux new-window -t $S -a -n emacs
+tmux send-keys -t $S:4 "cd ~/code/ggstr/alpha/" Enter
+tmux_env_python $S 4
+tmux_emacs $S 4
+
+tmux select-window -t $S:4
 tmux attach -t $S
