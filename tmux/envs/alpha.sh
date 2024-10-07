7a0066cb86ee5ccdaf0b50f241030560b06dd297
Omar Trejo <omar@mllabs.com>
2024-10-07 04:16:29 -0600

    Latest changes

--- tmux/envs/alpha.sh
+++ tmux/envs/alpha.sh
@@ -9,34 +9,38 @@ export S="alpha"
 tmux new-session -d -s $S
 tmux_email_ggstr $S
 
-tmux rename-window -t $S:1 sim-1
+tmux rename-window -t $S:1 db
 tmux send-keys -t $S:1 "cd ~/code/ggstr/alpha/svc-sim" Enter
 tmux_env_python $S 1
 
-tmux new-window -t $S -a -n sim-2
+tmux new-window -t $S -a -n sim-1
 tmux send-keys -t $S:2 "cd ~/code/ggstr/alpha/svc-sim" Enter
 tmux_env_python $S 2
 
-tmux new-window -t $S -a -n data-1
-tmux send-keys -t $S:3 "cd ~/code/ggstr/alpha/svc-data" Enter
+tmux new-window -t $S -a -n sim-2
+tmux send-keys -t $S:3 "cd ~/code/ggstr/alpha/svc-sim" Enter
 tmux_env_python $S 3
 
-tmux new-window -t $S -a -n data-2
+tmux new-window -t $S -a -n data-1
 tmux send-keys -t $S:4 "cd ~/code/ggstr/alpha/svc-data" Enter
 tmux_env_python $S 4
 
-tmux new-window -t $S -a -n app-1
-tmux send-keys -t $S:5 "cd ~/code/ggstr/alpha/svc-app" Enter
+tmux new-window -t $S -a -n data-2
+tmux send-keys -t $S:5 "cd ~/code/ggstr/alpha/svc-data" Enter
 tmux_env_python $S 5
 
-tmux new-window -t $S -a -n app-2
+tmux new-window -t $S -a -n app-1
 tmux send-keys -t $S:6 "cd ~/code/ggstr/alpha/svc-app" Enter
 tmux_env_python $S 6
 
-tmux new-window -t $S -a -n emacs
-tmux send-keys -t $S:7 "cd ~/code/ggstr/alpha/" Enter
+tmux new-window -t $S -a -n app-2
+tmux send-keys -t $S:7 "cd ~/code/ggstr/alpha/svc-app" Enter
 tmux_env_python $S 7
-tmux_emacs $S 7
 
-tmux select-window -t $S:7
+tmux new-window -t $S -a -n emacs
+tmux send-keys -t $S:8 "cd ~/code/ggstr/alpha/" Enter
+tmux_env_python $S 8
+tmux_emacs $S 8
+
+tmux select-window -t $S:8
 tmux attach -t $S
