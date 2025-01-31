fe23b9ad474be396faf86f73f0ef87704bfc7319
Omar Trejo <omar@mllabs.com>
2025-01-31 01:25:22 -0600

    Latest changes

--- tmux/envs/hs.sh
+++ tmux/envs/hs.sh
@@ -10,27 +10,23 @@ tmux new-session -d -s $S
 tmux_email_orth $S
 
 tmux rename-window -t $S:1 fe
-tmux send-keys -t $S:1 "cd ~/code/mll/hs/fe/" Enter
+tmux send-keys -t $S:1 "cd ~/code/mll/hs/insights-platform/fe/" Enter
 tmux_env_python $S 1
 
-tmux new-window -t $S:2 -a -n shared
-tmux send-keys -t $S:2 "cd ~/code/mll/hs/be-shr/" Enter
-tmux_env_python $S 2
-
-tmux new-window -t $S:3 -a -n svc-dvc-1
-tmux send-keys -t $S:3 "cd ~/code/mll/hs/be-dvc/" Enter
+tmux new-window -t $S:3 -a -n plg-1
+tmux send-keys -t $S:3 "cd ~/code/mll/hs/insights-plugins-mt-sinai-lvef/" Enter
 tmux_env_python $S 3
 
-tmux new-window -t $S:4 -a -n svc-dvc-2
-tmux send-keys -t $S:4 "cd ~/code/mll/hs/be-dvc/" Enter
+tmux new-window -t $S:4 -a -n plg-2
+tmux send-keys -t $S:4 "cd ~/code/mll/hs/insights-plugins-mt-sinai-lvef/" Enter
 tmux_env_python $S 4
 
-tmux new-window -t $S:5 -a -n svc-plt-1
-tmux send-keys -t $S:5 "cd ~/code/mll/hs/be-plt/" Enter
+tmux new-window -t $S:5 -a -n plt-1
+tmux send-keys -t $S:5 "cd ~/code/mll/hs/insights-platform/be/" Enter
 tmux_env_python $S 5
 
-tmux new-window -t $S:6 -a -n svc-plt-2
-tmux send-keys -t $S:6 "cd ~/code/mll/hs/be-plt/" Enter
+tmux new-window -t $S:6 -a -n plt-2
+tmux send-keys -t $S:6 "cd ~/code/mll/hs/insights-platform/be/" Enter
 tmux_env_python $S 6
 
 tmux new-window -t $S:7 -a -n emacs
