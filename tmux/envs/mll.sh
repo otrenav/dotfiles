98e04ec16e40844dda601654cb00cdfd169b954e
Omar Trejo <omar@mllabs.com>
2025-04-02 16:59:25 -0600

    Latest changes

--- tmux/envs/mll.sh
+++ tmux/envs/mll.sh
@@ -11,15 +11,12 @@ export S="mll"
 tmux new-session -d -s $S
 tmux_email_mll $S
 
-tmux rename-window -t $S:1 devops
+tmux rename-window -t $S:1 local
 tmux send-keys -t $S:1 "cd ~/code/mll/" Enter
 
-tmux new-window -t $S -a -n local
-tmux send-keys -t $S:2 "cd ~/code/mll/" Enter
-
 tmux new-window -t $S -a -n emacs
-tmux send-keys -t $S:3 "cd ~/code/mll/" Enter
-tmux_emacs $S 3
+tmux send-keys -t $S:2 "cd ~/code/mll/" Enter
+tmux_emacs $S 2
 
-tmux select-window -t $S:3
+tmux select-window -t $S:2
 tmux attach -t $S
