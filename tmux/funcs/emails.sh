2f93dbb47dd3e81d2bc46209c06e293a9b557cb3
Omar Trejo <omar@mllabs.com>
2025-04-15 23:16:44 -0600

    Latest changes

--- tmux/funcs/emails.sh
+++ tmux/funcs/emails.sh
@@ -1,8 +1,9 @@
 
 EMAIL_MLL="ML Labs: Omar Trejo (omar@mllabs.com) "
 EMAIL_PM="Pocket Made: Omar Trejo (omar@pocketmade.com) "
-EMAIL_GGSTR="Gigster: Omar Trejo (omar.trejo@gigsternetwork.com) "
 EMAIL_ORTH="Orthogonal: Omar Trejo (omar.trejo@orthogonal.io) "
+EMAIL_CB="CrowdBotics: Omar Trejo (omar.trejo@crowdbotics.com) "
+EMAIL_GGSTR="Gigster: Omar Trejo (omar.trejo@gigsternetwork.com) "
 
 function tmux_email_mll() {
     tmux set -t "$1" status-right "$EMAIL_MLL"
@@ -18,4 +19,8 @@ function tmux_email_ggstr() {
 
 function tmux_email_orth() {
     tmux set -t "$1" status-right "$EMAIL_ORTH"
+}
+
+function tmux_email_cb() {
+    tmux set -t "$1" status-right "$EMAIL_CB"
 }
