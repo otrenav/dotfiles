aab6493cb84c6035aa3d6d2ec2eb45fa35a6a560
Omar Trejo <omar@mllabs.com>
2025-05-14 01:46:25 -0600

    Latest changes

--- tmux/envs/hs.sh
+++ tmux/envs/hs.sh
@@ -18,7 +18,7 @@ tmux send-keys -t $S:2 "cd ~/code/mll/hs/insights-plugins/insights-plugins-mt-si
 tmux_env_python $S 2
 
 tmux new-window -t $S:3 -a -n plg-2
-tmux send-keys -t $S:3 "cd ~/code/mll/hs/insights-plugins/insights-plugins-mt-sinai-lvef-high-densenet121-v2/" Enter
+tmux send-keys -t $S:3 "cd ~/code/mll/hs/insights-plugins/insights-plugins-mt-sinai-lvef-high-densenet121-v1/" Enter
 tmux_env_python $S 3
 
 tmux new-window -t $S:4 -a -n plt-1
