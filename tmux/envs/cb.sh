cc3ae7c4f6eafbf6cd8364b2ec0b45f6ccd09785
Omar Trejo <omar@mllabs.com>
2025-05-14 20:22:34 -0600

    Latest changes

--- tmux/envs/cb.sh
+++ tmux/envs/cb.sh
@@ -9,18 +9,30 @@ export S="cb"
 tmux new-session -d -s $S
 tmux_email_cb $S
 
-tmux rename-window -t $S:1 lcl-1
-tmux send-keys -t $S:1 "cd ~/code/mll/cb" Enter
+tmux rename-window -t $S:1 nvtop
+tmux send-keys -t $S:1 "cd ~/code/mll/cb/gbc2s" Enter
 tmux_env_python $S 1
 
-tmux new-window -t $S -a -n lcl-2
-tmux send-keys -t $S:2 "cd ~/code/mll/cb" Enter
+tmux new-window -t $S -a -n llm-2
+tmux send-keys -t $S:2 "cd ~/code/mll/cb/gbc2s" Enter
 tmux_env_python $S 2
 
-tmux new-window -t $S:3 -a -n emacs
-tmux send-keys -t $S:3 "cd ~/code/mll/cb" Enter
+tmux new-window -t $S -a -n llm-2
+tmux send-keys -t $S:3 "cd ~/code/mll/cb/gbc2s" Enter
 tmux_env_python $S 3
-tmux_emacs $S 3
 
-tmux select-window -t $S:3
+tmux new-window -t $S -a -n llm-3
+tmux send-keys -t $S:4 "cd ~/code/mll/cb/gbc2s" Enter
+tmux_env_python $S 4
+
+tmux new-window -t $S -a -n llm-4
+tmux send-keys -t $S:5 "cd ~/code/mll/cb/gbc2s" Enter
+tmux_env_python $S 5
+
+tmux new-window -t $S:6 -a -n emacs
+tmux send-keys -t $S:6 "cd ~/code/mll/cb/gbc2s" Enter
+tmux_env_python $S 6
+tmux_emacs $S 6
+
+tmux select-window -t $S:6
 tmux attach -t $S
