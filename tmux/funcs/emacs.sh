8abcfb150669a289251cd436e517ec276571d014
Omar Trejo <omar@mllabs.com>
2025-06-10 21:45:00 -0600

    Latest changes

--- tmux/funcs/emacs.sh
+++ tmux/funcs/emacs.sh
@@ -1,7 +1,7 @@
 
 function tmux_emacs() {
     tmux send-keys -t $1:$2 "emacs ." Enter && \
-        sleep 0.5 && \
+        sleep 3 && \
         tmux send-keys -t $1:$2 "C-x" && \
         tmux send-keys -t $1:$2 "k" && \
         sleep 0.5 && \
