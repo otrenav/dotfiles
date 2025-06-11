d1ec9bb4c6cc3a57f9c24590bd245f242521c291
Omar Trejo <omar@mllabs.com>
2025-06-10 21:44:04 -0600

    Latest changes

--- tmux/funcs/emacs.sh
+++ tmux/funcs/emacs.sh
@@ -1,26 +1,27 @@
 
 function tmux_emacs() {
     tmux send-keys -t $1:$2 "emacs ." Enter && \
-        sleep 5 && \
+        sleep 0.5 && \
+        tmux send-keys -t $1:$2 "C-x" && \
+        tmux send-keys -t $1:$2 "k" && \
+        sleep 0.5 && \
+        tmux send-keys -t $1:$2 "C-x" && \
+        tmux send-keys -t $1:$2 "k" && \
+        sleep 0.5 && \
+        tmux send-keys -t $1:$2 "C-x" && \
+        tmux send-keys -t $1:$2 "k" && \
+        sleep 0.5 && \
+        tmux send-keys -t $1:$2 "C-x" && \
+        tmux send-keys -t $1:$2 "k" && \
+        sleep 0.5 && \
+        tmux send-keys -t $1:$2 "C-x" && \
+        tmux send-keys -t $1:$2 "k" && \
+        sleep 0.5 && \
         tmux send-keys -t $1:$2 "M-x" && \
         tmux send-keys -t $1:$2 "neotree" && \
+        tmux send-keys -t $1:$2 Enter && \
         sleep 0.5 && \
         tmux send-keys -t $1:$2 "C-o" && \
-        tmux send-keys -t $1:$2 "C-x" && \
-        tmux send-keys -t $1:$2 "k" && \
-        sleep 0.5 && \
-        tmux send-keys -t $1:$2 "C-x" && \
-        tmux send-keys -t $1:$2 "k" && \
-        sleep 0.5 && \
-        tmux send-keys -t $1:$2 "C-x" && \
-        tmux send-keys -t $1:$2 "k" && \
-        sleep 0.5 && \
-        tmux send-keys -t $1:$2 "C-x" && \
-        tmux send-keys -t $1:$2 "k" && \
-        sleep 0.5 && \
-        tmux send-keys -t $1:$2 "C-x" && \
-        tmux send-keys -t $1:$2 "k" && \
-        sleep 0.5 && \
         tmux send-keys -t $1:$2 "C-x" && \
         tmux send-keys -t $1:$2 "3" && \
         sleep 0.5 && \
