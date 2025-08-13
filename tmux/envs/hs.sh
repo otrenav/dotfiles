b44dc1dcd279f9961edf8417941dcd86fb333e2f
Omar Trejo <omar@mllabs.com>
2025-08-13 07:24:03 -0500

    Latest changes

--- tmux/envs/hs.sh
+++ tmux/envs/hs.sh
@@ -15,37 +15,32 @@ tmux send-keys -t $S:1 "C-l" Enter
 tmux send-keys -t $S:1 "git pull" Enter
 tmux send-keys -t $S:1 "npm run dev:lcl" Enter
 
-tmux new-window -t $S:2 -a -n fe-tests
-tmux send-keys -t $S:2 "cd ~/code/mll/hs/platform/qa/" Enter
+tmux new-window -t $S:2 -a -n mod-2
+tmux send-keys -t $S:2 "cd ~/code/mll/hs/models/insights-models-mt-sinai-lvef-low/" Enter
+tmux_env_python $S 2
 tmux send-keys -t $S:2 "C-l" Enter
-tmux send-keys -t $S:2 "npm test dev" Enter
 
-tmux new-window -t $S:3 -a -n mod-2
-tmux send-keys -t $S:3 "cd ~/code/mll/hs/models/insights-models-mt-sinai-lvef-low-densenet121-v1/" Enter
+tmux new-window -t $S:3 -a -n mod-1
+tmux send-keys -t $S:3 "cd ~/code/mll/hs/models/insights-models-mt-sinai-lvef-low/" Enter
 tmux_env_python $S 3
 tmux send-keys -t $S:3 "C-l" Enter
+tmux send-keys -t $S:3 "python main.py" Enter
 
-tmux new-window -t $S:4 -a -n mod-1
-tmux send-keys -t $S:4 "cd ~/code/mll/hs/models/insights-models-mt-sinai-lvef-low-densenet121-v1/" Enter
+tmux new-window -t $S:4 -a -n plt-1
+tmux send-keys -t $S:4 "cd ~/code/mll/hs/platform/be/" Enter
 tmux_env_python $S 4
 tmux send-keys -t $S:4 "C-l" Enter
 tmux send-keys -t $S:4 "python main.py" Enter
 
-tmux new-window -t $S:5 -a -n plt-1
+tmux new-window -t $S:5 -a -n plt-2
 tmux send-keys -t $S:5 "cd ~/code/mll/hs/platform/be/" Enter
 tmux_env_python $S 5
 tmux send-keys -t $S:5 "C-l" Enter
-tmux send-keys -t $S:5 "python main.py" Enter
 
-tmux new-window -t $S:6 -a -n plt-2
+tmux new-window -t $S:6 -a -n emacs
 tmux send-keys -t $S:6 "cd ~/code/mll/hs/platform/be/" Enter
 tmux_env_python $S 6
-tmux send-keys -t $S:6 "C-l" Enter
+tmux_emacs $S 6
 
-tmux new-window -t $S:7 -a -n emacs
-tmux send-keys -t $S:7 "cd ~/code/mll/hs/platform/be/" Enter
-tmux_env_python $S 7
-tmux_emacs $S 7
-
-tmux select-window -t $S:7
+tmux select-window -t $S:6
 tmux attach -t $S
